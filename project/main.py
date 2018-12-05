eb300f8a2f9480dd6e6b20bad1d53cdb99fdcbb8
Francisco Rodrigues <fx.rodrigues@campus.fct.unl.pt>
2018-12-05 20:21:31 +0000

    Adds normal python implementation of phase 2

--- project/main.py
+++ project/main.py
@@ -1,16 +1,21 @@
-# %%
+#%%
+import os.path
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-# Hide all warnings
+from tokenizer import tokenizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import euclidean_distances
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import linalg as LA
 import pandas as pd
-from skimage.feature import hog
-from skimage.color import rgb2gray
-from skimage.io import imread
 from sklearn.preprocessing import normalize
 from skimage import color
 from skimage import data, exposure
+from sklearn.metrics import precision_recall_fscore_support as score
+import matplotlib.pyplot as plt
+import sys
 import random
 from numpy.random import shuffle
 from sklearn.preprocessing import MultiLabelBinarizer
@@ -18,91 +23,90 @@ from sklearn.metrics import classification_report
 from skimage import img_as_ubyte
 import warnings
 import os
+import time
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input, decode_predictions
 from keras.preprocessing import image
+
 warnings.filterwarnings('ignore')
 
-print(os.getcwd(), "changing to:", os.getcwd()+"/../")
-
-# Change this according to the path where you have the ws_toolkit
-ws_toolkit_path = os.getcwd()+"/.."
-
-os.chdir(ws_toolkit_path)
-print(os.getcwd())
-from ws_toolkit.utils import center_crop_image, k_neighbours
+from ws_toolkit.utils import *
 
 # Cached arrays
 croppedImages = []
-
-def cropImageList(images):
-    global croppedImages
-    if len(croppedImages) <= 0:
-        for imgName in images:
-            croppedImg = []
-            # Read image
-            img = imread("./images/"+imgName)
-            # Resize image
-            croppedImg = center_crop_image(img, size=224)
-            croppedImages.append(croppedImg)
-    else:
-        print("Using cached cropped images")
-    return croppedImages
+X_HOC = None
+X_HOG = None
+X_BOW = None
+X_CNN = None
+CNN_PREDICTIONS = []
 
 
-def features_hog(images, pixels_per_cell=(32, 32), orientations=8):
-    gradientMatrix = []
-    for img in images:
-        # Convert to grayscale
-        img_gray = rgb2gray(img)
-        # Extract HoG features
-        hist = hog(img_gray, orientations=orientations,
-                   pixels_per_cell=pixels_per_cell)
-        # Normalize features
-        # We add 1 dimension to comply with scikit-learn API
-        hist = np.squeeze(normalize(hist.reshape(1, -1), norm="l2"))
-        gradientMatrix.append(hist)
-    # Creating a feature matrix for all images
-    gradientMatrix = np.array(gradientMatrix)
-    return gradientMatrix
+def computeFeatures():
+    global X_BOW, X_HOG, X_CNN, X_HOC, CNN_PREDICTIONS
+    pixels_per_cell = (32, 32)
+    orientations = 8
 
-def getColorMatrix(_croppedImages, _bins=(4,4,4), _hsv=True): 
-    colorMatrix = []
-    for img in _croppedImages:    
-        # Change image color space from RGB to HSV. 
-        # HSV color space was designed to more closely align with the way human vision perceive color-making attributes
-        img_q = img
-        if _hsv:
-            img_q = color.rgb2hsv(img)    
-        # convert image pixels to [0, 255] range, and to uint8 type
-        img_q = img_as_ubyte(img_q)
-        # Extract HoC features
-        hist, bin_edges = hoc(img_q, bins=_bins)    
-        # Normalize features
-        # We add 1 dimension to comply with scikit-learn API
-        hist = np.squeeze(normalize(hist.reshape(1, -1), norm="l2"))    
-        colorMatrix.append(hist)    
-    # Creating a feature matrix for all images
-    colorMatrix = np.array(colorMatrix)
-    return colorMatrix
+    bins = (4, 4, 4)
+    hsv = True
+
+    _lemmatize = False
+    _mdf = 3
+    _metric = "cosine"
+    _k = 10
+    _handles = False
+    _hashes = False
+    _case = False
+    _url = False
+
+    X_HOG = features_hog(X_HOG, croppedImages, pixels_per_cell, orientations)
+    X_HOC = features_hoc(X_HOC, croppedImages, bins, hsv)
+    X_BOW = features_bow(X_BOW, tweets, _lemmatize, _mdf,
+                         _metric, _k, _handles, _hashes, _case, _url)
+    X_CNN, CNN_PREDICTIONS = features_cnn(
+        X_CNN, CNN_PREDICTIONS, "./images/", imageLinks)
+
+#%%
+
+# Read dataset .csv
+df = pd.read_csv("./visualstories_edfest_2016_twitter_xmedia.csv",
+                 sep=';', encoding="utf-8")
+
+data = np.array([df.get("text").values, df.get(
+    "image-url").values, df.get("gt_class").values])
+# This are the text of the tweets
+tweets = data[0]
+# This are the links of the images of the tweets (ex: C0zsADasd213.jpg)
+imageLinks = [i.replace('https://pbs.twimg.com/media/', '') for i in data[1]]
+# This are the arrays of the data of each cropped image
+targets = [list(map(int, c.replace(' ', '').split(","))) for c in data[2]]
+# Save cropped images in cache
+croppedImages = cropImageList(croppedImages, imageLinks)
+
+# Cache features
+computeFeatures()
 
 
+def runAlg(mlb, images, y, y_true, features, weights, selection, topk, threshold, alpha, iterations, params=None, indices_unlabeled=[]):
 
-def runAlg(mlb, images, y, y_true, feature, alpha, iterations, sigma, params=None):
-    # Step 1 - Extract features for each image (HoG/CNN/HoC) in X
-    if feature is "hog":
-        pixels_per_cell = (32, 32)
-        orientations = 8
-        X = features_hog(images, pixels_per_cell, orientations)
-    else:
-        bins = (4,4,4)
-        hsv = True
-        X = getColorMatrix(images, bins, hsv)
-        
-    # Step 2 - Initialize matrix F with Y
-    F = y
+    # Step 2 - Normalize Y and Initialize matrix F with Y
+    Y_hidden = normalize(y, axis=1, norm="l1")
+    F = Y_hidden
+    # print(F[indices_labeled[0],:])
 
-    # Step 3 - Compute matrix W
-    from sklearn.metrics.pairwise import euclidean_distances
-    W = np.exp(-1*euclidean_distances(X, X)/(2*sigma**2))
+    # Step 3 - Compute matrix W (multi feature -mf true; or single feature -mf
+    # false)
+    W = []
+    M = 0
+    print("Features: {}".format(len(features)))
+    if(len(features) == 1):
+        weights[0] = 1
+    print(weights[0])
+    for i in range(len(features)):
+        M += weights[i] * euclidean_distances(features[i], features[i])
+    #M = weights[0]*euclidean_distances(feature[0], feature[0]) + weights[1]*euclidean_distances(feature[1], feature[1])
+
+    sigma = np.std(M)
+    W = np.exp(-1 * M / (2 * sigma**2))
 
     # Step 4 - Normalize W
     D = np.zeros(W.shape)
@@ -118,38 +122,32 @@ def runAlg(mlb, images, y, y_true, feature, alpha, iterations, sigma, params=Non
     # Step 5 - Perform the F update step num_iterations steps
     for i in range(1, iterations):
         T1 = alpha * S
+
         T1 = np.dot(T1, F)
-        T2 = 1 * alpha * y_true
+        T2 = (1 - alpha) * Y_hidden
         F = T1 + T2
+        # Normalizar para F (verficar segmentos)
+        F = normalize(F, axis=1, norm="l1")
+
+    print("Indice unlabeled: {}\nNormalized F: {}".format(
+        indices_unlabeled[0], F[indices_unlabeled[0], :]))
     # Select top k classes
-    F = np.fliplr(np.argsort(F, axis=1))
-    # Get the top 5 only classes
-    F = F[:,:5]
-    Y = mlb.transform(F)
+    if selection is True:
+        F = np.fliplr(np.argsort(F, axis=1))
+        F = F[:, :topk]
+        Y = mlb.transform(F)
+    else:
+        T = []
+        for row in F:
+            T.append([i for i, v in enumerate(row) if v >= threshold])
+        Y = mlb.transform(T)
     return Y
-   
 
 
-
-
-# Read dataset .csv
-df = pd.read_csv("./visualstories_edfest_2016_twitter_xmedia.csv",
-                 sep=';', encoding="utf-8")
-
-data = np.array([df.get("text").values, df.get(
-    "image-url").values, df.get("gt_class").values])
-# This are the text of the tweets
-tweets = data[0]
-# This are the links of the images of the tweets (ex: C0zsADasd213.jpg)
-imageLinks = [i.replace('https://pbs.twimg.com/media/', '') for i in data[1]]
-# This are the arrays of the data of each cropped image
-targets = [list(map(int, c.replace(' ', '').split(","))) for c in data[2]]
-# Save cropped images in cache
-croppedImages = cropImageList(imageLinks)
-
-# %%
-def runAll(iterations):
-    rand_seed = random.randint(1,100)
+def runAll(iterations, p, features, weights, alpha, selection, topk, threshold):
+    # Choose a random number between 1 and 100 to shuffle to prevent biased
+    # results
+    rand_seed = random.randint(1, 100)
     indices = np.arange(len(tweets))
     np.random.seed(rand_seed)
     shuffle(indices)
@@ -165,51 +163,77 @@ def runAll(iterations):
     total_images = X.shape[0]
 
     # Let's assume that 20% of the dataset is labeled
-    labeled_set_size = int(total_images*0.2)
+    labeled_set_size = int(total_images * p)
 
     indices_labeled = indices[:labeled_set_size]
     indices_unlabeled = indices[labeled_set_size:]
 
-    print("Total tweets labeled: {} - Total tweets unlabeled: {}".format(
-        len(indices_labeled), len(indices_unlabeled)))
-
+    print(" ")
+    print("Iteration: {} - Total tweets labeled: {} - Total tweets unlabeled: {}".format(iterations,
+                                                                                         len(indices_labeled), len(indices_unlabeled)))
 
     # Convert labels to a one-hot-encoded vector
     # Keep groundtruth labels
     classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     # print(classes)
     mlb = MultiLabelBinarizer(classes=classes)
+    # print(y_target[:1])
     Y_true = mlb.fit_transform(y_target)
     Y = mlb.transform(y_target)
-
+    # print(Y[:1])
     # Remove labels of "unlabeled" data
     Y[indices_unlabeled, :] = np.zeros(Y.shape[1])
 
     # Run Algorithm and Get Results
-    feature = "hog"
-    alpha = 1 # weight of the feature (in this case will be 1 = 100%)
-    sigma = 1
-    Y = runAlg(mlb, croppedImages, Y, Y_true, feature=feature,
-            alpha=alpha, iterations=iterations, sigma=sigma)
+    Y = runAlg(mlb, croppedImages, Y, Y_true, features=features, weights=weights, selection=selection, topk=topk, threshold=threshold,
+               alpha=alpha, iterations=iterations, indices_unlabeled=indices_unlabeled)
 
     Y_pred = Y[indices_unlabeled, :]
     y_gt = Y_true[indices_unlabeled, :]
+
+    print("Ground Truth: {}".format(Y_true[indices_unlabeled[0], :]))
+    print("Predicted:    {}".format(Y[indices_unlabeled[0], :]))
+
     return Y_pred, y_gt
 
-from sklearn.metrics import precision_recall_fscore_support as score
-
-results = np.zeros((49,4))
-range_iterations = range(10,500,10)
+results = np.zeros((10, 4))
+range_iterations = range(10, 500, 50)
 j = 0
 
+# Variable params
+p = 0.7
+# Possible X_HOG, X_HOG, X_BOW, X_CNN -> If mf=False use only one feature
+# inside the array
+features = [X_BOW, X_CNN]
+# Sum must be one - and must always be filled even if mf=False
+weights = [0.5, 0.5]
+alpha = 0.8
+selection = False  # False - Top K | True - Threshold
+topk = 3
+threshold = 0.15
+
 for i in range_iterations:
-    Y_pred, y_gt = runAll(i)
+    Y_pred, y_gt = runAll(i, p, features, weights, alpha,
+                          selection, topk, threshold)
     precision, recall, fscore, support = score(y_gt, Y_pred, average='macro')
     results[j] = [precision, recall, fscore, support]
-    j = j+1
+    j = j + 1
 
-# %%
-import matplotlib.pyplot as plt
-plt.plot(range_iterations, results[:,0], 'r', range_iterations, results[:,1], 'b', range_iterations, results[:,2], 'g')
+from sklearn.metrics import classification_report
+np.set_printoptions(threshold=np.nan)
 
-print(results)
+print("\nResults List:    \n{}".format(results))
+print("\nResults Report:  \n{}".format(classification_report(y_gt, Y_pred)))
+
+print("\nResults Graph:   \n")
+
+colors = ['r', 'b', 'g']
+labels = ['precision', 'recall', 'f-score']
+
+for i in range(3):
+    plt.plot(range_iterations, results[:, i], colors[i], label=labels[i])
+plt.xlabel("Iterations")
+plt.ylabel("Values")
+plt.legend(loc='best')
+plt.show()
+#plt.plot(range_iterations, results[:,0], 'r', range_iterations, results[:,1], 'b', range_iterations, results[:,2], 'g')
--- ws_toolkit/utils.py
+++ ws_toolkit/utils.py
@@ -1,3 +1,4 @@
+#%%
 from skimage.transform import resize
 from sklearn.metrics import pairwise_distances
 from skimage import color
@@ -10,27 +11,60 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.preprocessing import normalize
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input, decode_predictions
-
+from skimage.io import imread
+from skimage.color import rgb2gray
+from skimage.feature import hog
+from tokenizer import tokenizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import euclidean_distances
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import linalg as LA
+import pandas as pd
+from sklearn.preprocessing import normalize
+from skimage import color
+from skimage import data, exposure
+from sklearn.metrics import precision_recall_fscore_support as score
+import matplotlib.pyplot as plt
+import sys
+import random
+from numpy.random import shuffle
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import classification_report
+from skimage import img_as_ubyte
+import warnings
+import os
+import time
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+from keras.preprocessing import image
 
 def center_crop_image(im, size=224):
-
     if len(im.shape) >= 3 and im.shape[2] == 4: # Remove the alpha channel when present
         im = im[:, :, 0:3]
-
     # Resize so smallest dim = 224, preserving aspect ratio
     h, w, _ = im.shape
     if h < w:
         im = resize(image=im, output_shape=(224, int(w * 224 / h)))
     else:
         im = resize(im, (int(h * 224 / w), 224))
-
     # Center crop to 224x224
     h, w, _ = im.shape
     im = im[h // 2 - 112:h // 2 + 112, w // 2 - 112:w // 2 + 112]
-    
     return im
 
-
+def cropImageList(croppedImages, images):
+    if len(croppedImages) <= 0:
+        for imgName in images:
+            croppedImg = []
+            # Read image
+            img = imread("./images/"+imgName)
+            # Resize image
+            croppedImg = center_crop_image(img, size=224)
+            croppedImages.append(croppedImg)
+    else:
+        print("Using cached cropped images")
+    return croppedImages
 
 def k_neighbours(q, X, metric="euclidean", k=10):
     # Check pairwise_distances function docs: http://scikit-learn.org/stable/modules/generated/sklearn.metrics.pairwise_distances.html#sklearn.metrics.pairwise_distances
@@ -42,6 +76,7 @@ def k_neighbours(q, X, metric="euclidean", k=10):
     
     return sorted_indexes[:k], dists[sorted_indexes[:k]]
 
+# Histogram of Colors
 def hoc(im, bins=(16,16,16), hist_range=(256, 256, 256)):
     im_r = im[:,:,0]
     im_g = im[:,:,1]
@@ -61,6 +96,46 @@ def hoc(im, bins=(16,16,16), hist_range=(256, 256, 256)):
     
     return hist_r, bins_r
 
+def features_hoc(X_HOC, _croppedImages, _bins=(4,4,4), _hsv=True): 
+    #Histogram of colors results
+    if X_HOC is None:   
+        X_HOC = []
+        for img in _croppedImages:    
+            # Change image color space from RGB to HSV. 
+            # HSV color space was designed to more closely align with the way human vision perceive color-making attributes
+            img_q = img
+            if _hsv:
+                img_q = color.rgb2hsv(img)    
+            # convert image pixels to [0, 255] range, and to uint8 type
+            img_q = img_as_ubyte(img_q)
+            # Extract HoC features
+            hist, bin_edges = hoc(img_q, bins=_bins)    
+            # Normalize features
+            # We add 1 dimension to comply with scikit-learn API
+            hist = np.squeeze(normalize(hist.reshape(1, -1), norm="l2"))    
+            X_HOC.append(hist)    
+        # Creating a feature matrix for all images
+        X_HOC = np.array(X_HOC)
+    return X_HOC
+
+# Histogram of Gradients
+def features_hog(X_HOG, images, pixels_per_cell=(32, 32), orientations=8):
+    if X_HOG is None:
+        X_HOG = []
+        for img in images:
+            # Convert to grayscale
+            img_gray = rgb2gray(img)   
+            # Extract HoG features
+            hist = hog(img_gray, orientations=orientations, pixels_per_cell=pixels_per_cell)   
+            # Normalize features
+            # We add 1 dimension to comply with scikit-learn API
+            hist = np.squeeze(normalize(hist.reshape(1, -1), norm="l2"))  
+            X_HOG.append(hist)
+        # Creating a feature matrix for all images
+        X_HOG = np.array(X_HOG)
+    return X_HOG
+
+# Bag of Words
 def tokenizer_bow(sentence, tknzr, lemmatize=False):
     wnl = WordNetLemmatizer()
     tokens = []
@@ -78,6 +153,13 @@ def init_bow(texts, args, mdf):
     texts_bow = normalize(texts_bow, norm="l2")    
     return vectorizer, texts_bow
 
+def features_bow(X_BOW, _dataTexts, _lemmatize=False, _mdf=3, _metric="cosine", _k=10, _handles=False, _hashes=False, _case=False, _url=False):
+    if X_BOW is None:
+        tknzr = tokenizer.TweetTokenizer(preserve_handles=_handles, preserve_hashes=_hashes, preserve_case=_case, preserve_url=_url)
+        vectorizer, X_BOW = init_bow(_dataTexts, {"tknzr": tknzr, "lemmatize": _lemmatize}, _mdf)
+    return X_BOW
+
+# CNN - VGG 16
 def process_images_keras(_images, _folder="./images/"):
     processedImgs = []
     for imgId in _images:
@@ -94,3 +176,34 @@ def process_images_keras(_images, _folder="./images/"):
         x = preprocess_input(x)
         processedImgs.append(x)
     return processedImgs
+
+def features_cnn(X_CNN, CNN_PREDICTIONS, images_path, _dataImages):
+    model = VGG16(weights='imagenet', include_top=True)
+    start = time.time()
+    if len(CNN_PREDICTIONS) <= 0:
+        start_i = time.time()
+        img_list = process_images_keras(_dataImages, images_path)
+        end_i = time.time()
+        print("Processed Images finished: {}".format(end_i - start_i))
+        #model = ResNet50(weights='imagenet')
+        # Convert from list to ndarray
+        img_array_list = np.vstack(img_list)
+        # Feed all images to the model
+        print("No Cached Predictions")
+        CNN_PREDICTIONS = model.predict(img_array_list)
+        print("Finished dataset predictions")
+    else:
+        print("Using Cached Predictions")
+    end = time.time()
+    print("Model Predictions finished: {}".format(end - start))
+    #print("Resulting shape of the network output: {}".format(preds.shape))
+    concepts = decode_predictions(CNN_PREDICTIONS, top=5)
+    # Experiment with this parameter
+    k = 5
+    # Get the top K most probable concepts per image
+    sorted_concepts =  np.argsort(CNN_PREDICTIONS, axis=1)[:,::-1][:,:k]
+    data_tags = concepts
+    mlb = MultiLabelBinarizer(classes=range(0,1000))
+    X_CNN = mlb.fit_transform(sorted_concepts)
+    #print(tags_bow.shape)
+    return X_CNN, CNN_PREDICTIONS
